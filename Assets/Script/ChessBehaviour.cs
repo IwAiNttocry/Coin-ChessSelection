c9ec0e727dc54ee72d71c1a795c98ed747f6f32b
SACHI_IWTC <pulawat1704limsila@gmail.com>
2026-05-18 09:13:19 +0700

    Hover

--- Assets/Script/ChessBehaviour.cs
+++ Assets/Script/ChessBehaviour.cs
@@ -1,39 +1,19 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using UnityEngine .EventSystems;
+using UnityEngine.UI;
+
 
 public class ChessBehaviour : MonoBehaviour
 {
-    private Transform  _selection;
-    private RaycastHit _raycastHit;
-
-    [SerializeField] private float liftHeight = 1f;
-    [SerializeField] private float liftSpeed  = 5f;
-
-    private Vector3 _originalPosition;
-    private bool    _isSelected;
+    private Transform selection;
+    private RaycastHit raycasthit;    
+    Vector2 mousePosition;  
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        mousePosition = Input.mousePosition;
+        print(mousePosition);
 
-        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out _raycastHit))
-        {
-            _selection = _raycastHit.transform;
-
-            if (_selection.CompareTag("Selectable") && Input.GetMouseButtonDown(0))
-            {
-                _isSelected      = !_isSelected;
-                _originalPosition = new Vector3(_selection.position.x, 0.78f, _selection.position.z);
-            }
-        }
-
-        if (_selection != null)
-        {
-            Vector3 targetPos = _isSelected
-                ? _originalPosition + Vector3.up * liftHeight
-                : _originalPosition;
-
-            _selection.position = Vector3.Lerp(_selection.position, targetPos, Time.deltaTime * liftSpeed);
-        }
     }
 }
--- Assets/Script/SelectChess.cs
+++ Assets/Script/SelectChess.cs
@@ -3,18 +3,30 @@ using UnityEngine.EventSystems;
 
 public class SelectChess : MonoBehaviour
 {
-    
     [SerializeField] private Camera mainCamera;
-
 
     void Update()
     {
+        // Hover
+        Ray hoverRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hoverHits = Physics.RaycastAll(hoverRay, Mathf.Infinity);
+
+        if (hoverHits.Length > 0)
+        {
+            print("Is Hover");
+        }
+        else
+        {
+            print("Isn't Hover");
+        }
+
+        // Click
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity);
+            Ray clickRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] clickHits = Physics.RaycastAll(clickRay, Mathf.Infinity);
 
-            if (hits.Length > 0)
+            if (clickHits.Length > 0)
             {
                 print("Click");
             }
